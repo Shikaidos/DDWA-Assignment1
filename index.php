1afc4ab7842a06653b58e522d8ce3b887c3a6e27
Bing Heng <s10195248@connect.np.edu.sg>
2020-12-13 17:57:06 +0800

    login page validation

--- index.php
+++ index.php
@@ -1,198 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 550px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-        
-    /* On small screens, set height to 'auto' for the grid */
-    @media screen and (max-width: 767px) {
-      .row.content {height: auto;} 
-    }
-  </style>
-  <?php
-  // 1. Create a database connection
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpass = "";
-  $dbname = "ddwa_assignment1";
-  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-  // Test if connection occurred.
-  if(mysqli_connect_errno()) {
-    die("Database connection failed: " . 
-         mysqli_connect_error() . 
-         " (" . mysqli_connect_errno() . ")"
-    );
-  }
-  else{ //continued within body tag
-  
-?>
+	<title>Login V5</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+				<form class="login100-form validate-form flex-sb flex-w">
+					<span class="login100-form-title p-b-53">
+						Sign In
+					</span>
+					
+					<div class="p-t-31 p-b-9">
+						<span class="txt1">
+							Username
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Username is required">
+						<input id="userInput" class="input100" type="text" name="username" >
+						<span class="focus-input100"></span>
+					</div>
+					
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Password
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input id="userPassword" class="input100" type="password" name="pass" >
+						<span class="focus-input100"></span>
+					</div>
 
-<nav class="navbar navbar-inverse visible-xs">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Dashboard</a></li>
-        <li><a href="#">Age</a></li>
-        <li><a href="#">Gender</a></li>
-        <li><a href="#">Geo</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+					
 
-<div class="container-fluid">
-  <div class="row content">
-    <div class="col-sm-3 sidenav hidden-xs">
-      <h2>Logo</h2>
-      <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#section1">Dashboard</a></li>
-        <li><a href="#section2">Age</a></li>
-        <li><a href="#section3">Gender</a></li>
-        <li><a href="#section3">Geo</a></li>
-      </ul><br>
-    </div>
-    <br>
-    
-    <div class="col-sm-9">
-      <div class="well">
-        <h4><?php 
-            $sql = "SELECT name "; //Remember spacing! If not SQL string will be stuck tog.
-            $sql .= "FROM student "; //Reading from "Mutant" table
-            $sql .= "ORDER BY name;"; //Alphabetical order
-            $result = $connection->query($sql);
-            
-            if ($result->num_rows > 0) {
-            // output data of each row
-            
-            while($row = mysqli_fetch_assoc($result)) {
-                echo $row["name"]."<br/>";
-            }
-        } 	
-	?></h4>
-        <p><?php 
-            $sql = "SELECT student_id "; //Remember spacing! If not SQL string will be stuck tog.
-            $sql .= "FROM student "; //Reading from "Mutant" table
-            $sql .= "ORDER BY name;"; //Alphabetical order
-            $result = $connection->query($sql);
-            
-            if ($result->num_rows > 0) {
-            // output data of each row
-            
-            while($row = mysqli_fetch_assoc($result)) {
-                echo $row["student_id"]."<br/>";
-            }
-        } 	else {
-            echo "No results!";
-        }
+                </form>
+                <div class="container-login100-form-btn m-t-17">
+						<button onclick="validate()" class="login100-form-btn">
+							Sign In
+						</button>
+					</div>
+			</div>
+		</div>
+	</div>
+	
 
-            }
-    ?></p>
-    <p><?php 
-            $sql = "SELECT contact_no "; //Remember spacing! If not SQL string will be stuck tog.
-            $sql .= "FROM student "; //Reading from "Mutant" table
-            $sql .= "ORDER BY name;"; //Alphabetical order
-            $result = $connection->query($sql);
-            
-            if ($result->num_rows > 0) {
-            // output data of each row
-            
-            while($row = mysqli_fetch_assoc($result)) {
-                echo $row["student_id"]."<br/>";
-            }
-        } 	else {
-            echo "No results!";
-        }
-
-            }
-	?></p>
-      </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Users</h4>
-            <p>1 Million</p> 
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Pages</h4>
-            <p>100 Million</p> 
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Sessions</h4>
-            <p>10 Million</p> 
-          </div>
-        </div>
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Bounce</h4>
-            <p>30%</p> 
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="well">
-            <p>Text</p> 
-            <p>Text</p> 
-            <p>Text</p> 
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="well">
-            <p>Text</p> 
-            <p>Text</p> 
-            <p>Text</p> 
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="well">
-            <p>Text</p> 
-            <p>Text</p> 
-            <p>Text</p> 
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-8">
-          <div class="well">
-            <p>Text</p> 
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="well">
-            <p>Text</p> 
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="js/scripts.js"></script>
 
 </body>
 </html>
